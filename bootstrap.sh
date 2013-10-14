9ccc0982fcbe78d8cf53709912573602d447da45
linus <linus@linus.(none)>
2013-10-14 15:10:47 +0900

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/env bash
+############################  SETUP PARAMETERS
+app_name='config-git'
+git_uri='https://github.com/ygpark/config-git'
+git_branch='master'
+debug_mode='0'
+fork_maintainer='0'
+
+############################  BASIC SETUP TOOLS
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    if [ "$ret" -eq '0' ]; then
+    msg "\e[32m[✔]\e[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\e[31m[✘]\e[0m ${1}${2}"
+    exit 1
+}
+
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+      msg "An error occured in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
+    fi
+}
+
+program_exists() {
+    local ret='0'
+    type $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # throw error on non-zero return value
+    if [ ! "$ret" -eq '0' ]; then
+    error "$2"
+    fi
+}
+
+############################ SETUP FUNCTIONS
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+    ret="$?"
+    debug
+}
+
+do_backup() {
+    if [ -e "$2" ] || [ -e "$3" ] || [ -e "$4" ]; then
+        today=`date +%Y%m%d_%s`
+        for i in "$2" "$3" "$4"; do
+            [ -e "$i" ] && [ ! -L "$i" ] && mv "$i" "$i.$today";
+        done
+        ret="$?"
+        success "$1"
+        debug
+   fi
+}
+
+upgrade_repo() {
+      msg "trying to update $1"
+
+      if [ "$1" = "$app_name" ]; then
+          cd "$HOME/.$app_name" &&
+          git pull origin "$git_branch"
+      fi
+
+      if [ "$1" = "vundle" ]; then
+          cd "$HOME/.vim/bundle/vundle" &&
+          git pull origin master
+      fi
+
+      ret="$?"
+      success "$2"
+      debug
+}
+
+clone_repo() {
+    program_exists "git" "Sorry, we cannot continue without GIT, please install it first."
+    endpath="$HOME/.$app_name"
+
+    if [ ! -e "$endpath/.git" ]; then
+        git clone --recursive -b "$git_branch" "$git_uri" "$endpath"
+        ret="$?"
+        success "$1"
+        debug
+    else
+        upgrade_repo "$app_name"    "Successfully updated $app_name"
+    fi
+}
+
+clone_vundle() {
+    if [ ! -e "$HOME/.vim/bundle/vundle" ]; then
+        git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
+    else
+        upgrade_repo "vundle"   "Successfully updated vundle"
+    fi
+    ret="$?"
+    success "$1"
+    debug
+}
+
+create_symlinks() {
+    endpath="$HOME/.$app_name"
+
+    lnif "$endpath/.gitconfig"          "$HOME/.gitconfig"
+
+    ret="$?"
+    success "$1"
+    debug
+}
+
+setup_vundle() {
+    system_shell="$SHELL"
+    export SHELL='/bin/sh'
+    vim -u "$HOME/.vimrc.bundles" +BundleInstall! +BundleClean +qall
+    export SHELL="$system_shell"
+
+    success "$1"
+    debug
+}
+
+############################ MAIN()
+
+program_exists "git" "To install $app_name you first need to install Git."
+
+clone_repo      "Successfully cloned $app_name"
+
+create_symlinks "Setting up vim symlinks"
+
+do_backup   "Your old git stuff has a suffix now and looks like .gitconfig.`date +%Y%m%d%S`" \
+        "$HOME/.gitconfig"
+
+msg             "\nThanks for installing $app_name."
+msg             "© `date +%Y` http://github.com/ygpark/config-git"
+
