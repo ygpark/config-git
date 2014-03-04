3a17d5751fbe9b4bdfc12b061c063f0e8f1d6f21
linus <ghostyak@gmail.com>
2014-03-04 09:30:25 +0900

    add some bash functions

--- _bashrc.sh
+++ _bashrc.sh
@@ -7,3 +7,14 @@ function git-ps1-off
 {
     source $HOME/.config-git/script/ps1-off.sh
 }
+
+function search_cs
+{
+    #find . -type f -regex ".*\(cs\|resx\)" -print0 | xargs -0 grep -nH $1 --color=auto
+    find "$1" -type f -regex ".*\(cs\)" -print0 | xargs -0 grep -nH "$2" --color=auto
+}
+
+function search_cs_f
+{
+    search_cs $* | sed -e 's/:.*$//g' | sort -u
+}
