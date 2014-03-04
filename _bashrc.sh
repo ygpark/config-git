733caf77391c95ce16ba1f59bb96bbb6b4a3f95d
linus <ghostyak@gmail.com>
2014-03-04 13:04:42 +0900

    add help for search_cs

--- _bashrc.sh
+++ _bashrc.sh
@@ -8,10 +8,28 @@ function git-ps1-off
     source $HOME/.config-git/script/ps1-off.sh
 }
 
+function _search
+{
+    find "$1" -type f -regex $2 -print0 | xargs -0 grep -nH "$3" --color=auto
+}
+
+#Brief:
+#Usage: search_cs <keyword>
+#Usage: search_cs <dir> <keyword>
 function search_cs
 {
-    #find . -type f -regex ".*\(cs\|resx\)" -print0 | xargs -0 grep -nH $1 --color=auto
-    find "$1" -type f -regex ".*\(cs\)" -print0 | xargs -0 grep -nH "$2" --color=auto
+    if [ "$1" = "help" ];then
+        echo "Usage:"
+        echo "    search_cs <keyword>"
+        echo "    search_cs <dir> <keyword>"
+        return 0;
+    fi
+
+    if [ "$#" = "1" ]; then
+        _search "." ".*\(cs\)" "$1"
+    elif [ "$#" = "2" ]; then
+        _search "$1" ".*\(cs\)" "$2"
+    fi
 }
 
 function search_cs_f
